8e6e45c43535abc9f9652bc3ce4cef7884d193d9
Nasir Ahmad Noori <nnoori@Nasirs-MacBook-Pro.local>
2022-06-07 14:46:53 +0430

    First Commit

---
+++ lib/keys.dart
@@ -0,0 +1,1 @@
+const String API_KEY = "AIzaSyDf5j3_UlgNdZfi0i2OXJcL6oO3NHwJgEM";
---
+++ lib/services/remote_service.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+
+import '../models/post_image.dart';
+
+class RemoteService {
+  Future<List<Post>?> getPosts() async {
+    var client = http.Client();
+
+    var uri = Uri.parse('https://jsonplaceholder.typicode.com/photos');
+    var response = await client.get(uri);
+    if (response.statusCode == 200) {
+      var json = response.body;
+      return postFromJson(json);
+    }
+    return null;
+  }
+}
